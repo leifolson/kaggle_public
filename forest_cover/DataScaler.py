c75a024e10ab2425759f22a42c9637cb2bbee4ce
Clinton Olson <clint.olson2@gmail.com>
2014-09-25 11:41:26 -0700

    Added directory for forest cover kaggle practice competition Also added a DataScaler class for scaling/normalizing data

---
+++ forest_cover/DataScaler.py
@@ -0,0 +1,37 @@
+'''
+    The DataScaler class provides a number of data scaling techniques
+    for array-like objects where rows are samples and columns are 
+    variables
+'''
+import pandas as pd
+
+class DataScaler:
+
+    # init object
+    def __init__(self,data):
+        self.data = pd.DataFrame(data)
+
+    # range scaling
+    def rangeScale(self,cols):
+        # get min/max values
+        minVals = self.data.ix[:,cols].min()
+        maxVals = self.data.ix[:,cols].max()
+
+        # compute range
+        rangeVals = maxVals - minVals
+
+        # scale the features
+        return ((self.data.ix[:,cols] - minVals) / rangeVals)
+
+        
+
+    # zero mean, unit variance scaling
+
+
+    # get original data
+    def getData(self):
+        return self.data
+
+    # set data
+    def setData(self,data):
+        self.data = pd.DataFrame(data)
