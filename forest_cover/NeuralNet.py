d36f2aac7d083276eb34625b382610b5f5d8e1b4
Clinton Olson <clint.olson2@gmail.com>
2014-10-21 09:30:01 -0700

    added initWeights method to initialize network weights

--- forest_cover/NeuralNet.py
+++ forest_cover/NeuralNet.py
@@ -5,6 +5,10 @@
     The NeuralNet class implements a neural network model.
     The network can have an arbitrary number of layers of a specified size.  
     An overriding goal is to provide a general implementation
+
+    Data, X, is assumed to have examples in columns.
+    i.e. if we have N training examples, each of dimension M, then
+    X will be MxN
 '''
 
 import numpy as np
@@ -21,16 +25,22 @@ class NeuralNet:
         self.epochs = epochs
 
         # initialize small random network weights
+        self.weights = self.initWeights(layers)
 
 
     '''
     Train Network
     '''
+    def trainNet(self,data,eta):
+
+        return
 
     '''
-    Test Network
+    Network Predict
     '''
+    def netPredict(self,data):
 
+        return
 
     '''
     Forward propagation
@@ -43,8 +53,33 @@ class NeuralNet:
     '''
     Set epochs
     '''
+    def setEpochs(self,epochs):
+        self.epochs = epochs
     
     '''
     Set layers
     '''
+    def setLayers(self,layers):
+        self.layers = layers
+        self.weights = self.initWeights(layers)
+
+    '''
+    Init weights
+
+        Weights are initialized to small random pos/neg numbers.
+        The weights for each layer are stored in a list where 
+        list element corresponds to the layer of the same index       
+    '''
+    def initWeights(self,layers):
+
+        # for each layer, create the corresponding weights matrix
+        # and store into the list
+        weights = []
+        
+        for i in range(len(layers)-1):
+            rowDim = layers[i]
+            colDim = layers[i+1]
+            weights.append(np.random.rand(rowDim+1,colDim)*0.1-0.05)
+
+        return weights
 
